cd8611ceae45d59cdb5c0c21e1f4fc5cd751095f
TomoChainC <96672471+TomoChainC@users.noreply.github.com>
2021-12-28 00:23:47 -0500

    Update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -9,7 +9,7 @@ export default {
     mixins: [mixin],
     head () {
         return {
-            title: 'TomoScan - TomoChain Block Explorer'
+            title: 'TomoScan - Explorer'
         }
     },
     data: () => ({}),
